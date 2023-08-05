415840cf4d245b12811e544a4b41d260f281e16a
kidoyoshiyuki <kido@test>
2023-08-05 09:12:30 +0900

    modify seeder

--- database/seeders/Categories.php
+++ database/seeders/Categories.php
@@ -13,22 +13,22 @@ class Categories extends Seeder
      */
     public function run(): void
     {
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '雑誌'
         ]);
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '小説'
         ]);
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '漫画'
         ]);
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '専門書'
         ]);
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '実用書'
         ]);
-        DB::table('Categories')->insert([
+        DB::table('categories')->insert([
             'name' => '写真集'
         ]);
     }
